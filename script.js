21e47906130875ed59e2bbd271fb2c515568aa6d
senuracmps@gmail.com <senuracmps@gmail.com>
2026-06-27 19:10:29 +0530

    back to top button

--- script.js
+++ script.js
@@ -16,3 +16,20 @@ themeBtn.addEventListener("click", () => {
         themeBtn.textContent = "DARK";
     }
 });
+
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+  }
+});
+
+topBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
